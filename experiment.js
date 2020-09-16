10f96ee1dc93e0bbdbbf9ffdf467ee32b8c4b525
LUPHVU1 <tabitha70@hotmail.com>
2020-09-16 20:48:35 +0200

    add cube1 x

--- experiment.js
+++ experiment.js
@@ -73,7 +73,7 @@ function init() {
         new THREE.BoxBufferGeometry(1,1,1),
         new THREE.MeshLambertMaterial({colo:'green'})
     );
-    cube.position.set(-4, 1.5, -15);
+    cube.position.set(-8, 3.5, -5);
     scene.add(cube1);
     arObjects.push(cube1);
 
